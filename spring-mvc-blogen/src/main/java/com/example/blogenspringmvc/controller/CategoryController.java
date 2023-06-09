9d0fb08ee52534e9a4a6488b2390b7e9fa1b0e91
Zayar Htun <zayarhtun2092600@gmail.com>
2023-06-09 20:53:56 +0630

    Blogen Spring MVC

---
+++ spring-mvc-blogen/src/main/java/com/example/blogenspringmvc/controller/CategoryController.java
@@ -0,0 +1,36 @@
+package com.example.blogenspringmvc.controller;
+
+import com.example.blogenspringmvc.entity.Category;
+import com.example.blogenspringmvc.entity.Users;
+import com.example.blogenspringmvc.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.naming.Binding;
+
+@Controller
+public class CategoryController {
+    @GetMapping("/list-category")
+    public String listCategories(Model model){
+        model.addAttribute("categories",
+                categoryService.findAllCategory());
+        return "categories";
+
+    }
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping("/save-category")
+    public String saveCategory(Category category, BindingResult result){
+        if(result.hasErrors()){
+            return "home";
+        }
+        categoryService.saveCategory(category);
+        return "redirect:/";
+    }
+}
---
+++ spring-mvc-blogen/src/main/java/com/example/blogenspringmvc/service/UsersService.java
@@ -0,0 +1,21 @@
+package com.example.blogenspringmvc.service;
+
+import com.example.blogenspringmvc.dao.UsersDao;
+import com.example.blogenspringmvc.entity.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersService {
+    @Autowired
+    private UsersDao usersDao;
+
+    public void saveUser(Users user){
+        usersDao.save(user);
+    }
+    public List<Users> findAllUser(){
+        return usersDao.findAll();
+    }
+}
